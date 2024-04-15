8653afd010198660a0669811f89d74099a2bd111
Cynthia Apura <120573517+cynthiaapura@users.noreply.github.com>
2024-04-15 22:56:39 +0200

    logo contact + date JS

---
+++ src/app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", e => {
+    let dates_footer;
+
+    dates_footer = document.querySelector("footer p time");
+
+    const version = new Date().getFullYear();
+    dates_footer.innerText = version;
+    
+});
