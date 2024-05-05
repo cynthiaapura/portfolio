8b1d0c9f9ed0be3589c3892ad3c22d4abb7c1aa0
Cynthia Apura <120573517+cynthiaapura@users.noreply.github.com>
2024-05-05 17:08:39 +0200

    js image

--- js/app.js
+++ js/app.js
@@ -5,4 +5,18 @@ document.addEventListener("DOMContentLoaded", e => {
 
     const version = new Date().getFullYear();
     dates_footer.innerText = version;
+
+    const projectLinks = document.querySelectorAll('.project-link');
+
+    projectLinks.forEach(link => {
+        link.addEventListener('mouseenter', () => {
+            link.querySelector('img').style.filter = 'brightness(0) invert(1)';
+            link.querySelector('.overlay').style.opacity = '1';
+        });
+
+        link.addEventListener('mouseleave', () => {
+            link.querySelector('img').style.filter = 'none';
+            link.querySelector('.overlay').style.opacity = '0';
+        });
+    });
 });
